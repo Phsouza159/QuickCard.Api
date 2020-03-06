d34d17b074fc96e208e1bee6c4d95adf317a58d5
paulo <pauloh159@live.com>
2020-03-06 01:55:40 -0300

    add synchronismSerice

--- src/app/controllers/synchronismController.js
+++ src/app/controllers/synchronismController.js
@@ -1,55 +1,27 @@
 const express = require('express')
     , authMiddleware = require('@middlewares/autenticacao')
-    , Card = require('@models/card')
-    , Deck = require('@models/deck')
-    , Note = require('@models/note')
-    , NotePad = require('@models/notePad')
-    , Student = require('@models/student')
+    , SynchronismServe = require('@service/synchronismService')
     , BadRequestException = require('@exception/badRequestException')    
     , base = require('./baseController')
-    , codHttp = require('@enum/codHttp')
     , router = express.Router()
     , pathRoute = require('@config/router')
     , pathSynchronism = pathRoute.v1.synchronism
 
 const synchronismController = ( (app) => {
 
+    // authentication
+    if (!pathSynchronism.allowanonymous)
+        router.use(authMiddleware);
+
     router.get(`${pathSynchronism.get}` , async (req , res) => {
         try{
             const id = req.params.id
 
-            const student = await Student.findById(id)
+            var synchronismServe = new SynchronismServe()
 
-            if(!student)
-                throw new BadRequestException('Student is not exist')
+            let response = await synchronismServe.getSync(id)
 
-            const decks = await Deck.find({ 'student' : student.id })
-
-            if(decks)
-            {
-                for(let i = 0; i < decks.length ; i += 1)
-                {
-                    let deck = decks[i]
-                    deck.card = await Card.find({'deck':deck.id})
-                }
-            }
-
-            const notePads = await NotePad.find({ 'student' : student.id })
-
-            if(notePads)
-            {
-                for(let i = 0; i < notePads.length ; i += 1)
-                {
-                    let notePad = notePads[i]
-                    notePad.note = await Note.find({'notePad':notePad.id})
-                }
-            }
-
-            res.send({
-                student
-                , decks
-                , notePads
-            })
+            res.send(response)
         }
         catch(err){
             base.error(res, err)
---
+++ src/app/service/synchronismService.js
@@ -0,0 +1,48 @@
+const Card = require('@models/card')
+    , Deck = require('@models/deck')
+    , Note = require('@models/note')
+    , NotePad = require('@models/notePad')
+    , Student = require('@models/student')
+
+const SynchronismServe = (function(){
+
+
+})
+
+SynchronismServe.prototype.getSync = async idStudent => {
+
+    const student = await Student.findById(idStudent)
+
+    if(!student)
+        throw new BadRequestException('Student is not exist')
+
+    const decks = await Deck.find({ 'student' : student.id })
+
+    if(decks)
+    {
+        for(let i = 0; i < decks.length ; i += 1)
+        {
+            let deck = decks[i]
+            deck.card = await Card.find({'deck':deck.id})
+        }
+    }
+
+    const notePads = await NotePad.find({ 'student' : student.id })
+
+    if(notePads)
+    {
+        for(let i = 0; i < notePads.length ; i += 1)
+        {
+            let notePad = notePads[i]
+            notePad.note = await Note.find({'notePad':notePad.id})
+        }
+    }
+
+    return {
+        student
+        , decks
+        , notePads
+    }
+}
+
+module.exports = SynchronismServe
